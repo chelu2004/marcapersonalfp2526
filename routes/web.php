2168c49b71af2bdc9604203b809e5e8912525885
chelu2004 <chelu2.02004@gmail.com>
2025-11-05 08:07:40 +0100

    Ejercicios de rutas resuelto

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,51 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/', function()
+{
+    return 'Pantalla principal';
 });
+
+
+Route::get('/login', function()
+{
+    return 'Login usuario';
+});
+
+Route::get('/logout', function()
+{
+    return 'Logout usuario';
+});
+
+Route::get('/proyectos', function()
+{
+    return 'Listado proyectos';
+});
+
+Route::get('/proyectos/show/{id}', function($id)
+{
+    if (isset($id) && is_numeric($id)) {
+    return 'Detalle del proyecto: ' . $id;
+    } else {
+        return 'ID de proyecto no válido';
+    }
+})->where('id', '[0-9]+');
+
+Route::get('proyectos/edit/{id}', function($id)
+{
+    if (isset($id) && is_numeric($id)) {
+    return 'Editar proyecto: ' . $id;
+    } else {
+        return 'ID de proyecto no válido';
+    }
+})->where('id', '[0-9]+');
+
+Route::get('perfil/{id}', function($id)
+{
+    if (isset($id) && is_numeric($id)) {
+    return 'Perfil del usuario: ' . $id;
+    } else {
+        return 'ID de usuario no válido';
+    }
+})->where('id', '[0-9]+');
